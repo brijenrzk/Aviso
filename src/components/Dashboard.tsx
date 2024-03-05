e687b5ee320b1e9fb65c24e59e97f14f5d52fc97
Brijen Rajak <brijenrajak@gmail.com>
2024-03-05 20:14:07 +0530

    Built File uploder

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,15 +1,32 @@
 "use client"
 import { trpc } from "@/app/_trpc/client";
 import UploadButton from "./UploadButton";
-import { GhostIcon, MessageSquare, Plus, Trash } from "lucide-react";
+import { GhostIcon, Loader2, MessageSquare, Plus, Trash } from "lucide-react";
 import Skeleton from "react-loading-skeleton"
 import Link from "next/link";
 import { format } from "date-fns";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 const Dashboard = () => {
 
+    const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<string | null>(null)
+
+    const utils = trpc.useContext()
+
     const { data: files, isLoading } = trpc.getUserFiles.useQuery()
+
+    const { mutate: deleteFile } = trpc.deleteFile.useMutation({
+        onSuccess: () => {
+            utils.getUserFiles.invalidate()
+        },
+        onMutate({ id }) {
+            setCurrentlyDeletingFile(id)
+        },
+        onSettled() {
+            setCurrentlyDeletingFile(null)
+        }
+    })
     return <main className="mx-auto max-w-7xl md:p-10">
         <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
             <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
@@ -40,7 +57,13 @@ const Dashboard = () => {
                                 <MessageSquare className="h-4 w-4" />
                                 10
                             </div>
-                            <Button variant='destructive' size='sm' className="w-full"><Trash className="h-4 w-4" /></Button>
+                            <Button variant='destructive' size='sm'
+                                onClick={() => deleteFile({ id: file.id })}
+                                className="w-full">
+                                {currentlyDeletingFile === file.id ? (
+                                    <Loader2 className="h-4 w-4 animate-spin" />
+                                ) : <Trash className="h-4 w-4" />}
+                            </Button>
                         </div>
 
                     </li>
