e0398fef7dd331f0da670cb3c785278188abae36
Brijen Rajak <brijenrajak@gmail.com>
2024-03-07 16:06:04 +0530

    fixed err 3

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,7 +4,6 @@ import { PropsWithChildren, useState } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { trpc } from "@/app/_trpc/client"
 import { httpBatchLink } from "@trpc/client"
-import { absoluteUrl } from "@/lib/utils"
 
 const Providers = ({ children }: PropsWithChildren) => {
     const [queryClient] = useState(() => new QueryClient())
