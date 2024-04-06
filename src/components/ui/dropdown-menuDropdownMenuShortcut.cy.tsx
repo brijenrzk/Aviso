899239cd3b54b82ec93fa726b1431e8a5fefa305
Brijen Rajak <brijenrajak@gmail.com>
2024-04-06 17:00:01 +0530

    Fixed few bugs

--- src/components/ui/dropdown-menuDropdownMenuShortcut.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { DropdownMenuShortcut } from './dropdown-menu'
-
-describe('<DropdownMenuShortcut />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<DropdownMenuShortcut />)
-  })
-})
