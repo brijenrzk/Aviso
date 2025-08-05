56eabadbb3e222a3d5166ba6235095a835d5c23e
brijenrzk <brijenrajak@gmail.com>
2025-08-05 23:47:08 +0530

    Fixed absolutue url for deploymnet

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,7 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') return path
   if (process.env.VERCEL_URL) {
-    return `https://aviso-flame.vercel.app`
+    return `https:///aviso-flame.vercel.app${path}`
   }
   return `http://localhost:${process.env.PORT ?? 3000
     }${path}`
